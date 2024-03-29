331d25d9f47491ee07bff97800e38e4c12346d8a
Billy Dunn <bdunn27@gmail.com>
2024-03-29 16:00:26 -0400

    react hooks

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <div>{<h1>please pass this test</h1>}</div>;
 }
 
 export default Article;
